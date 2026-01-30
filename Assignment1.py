d0b4cbe87f610aee3aa59af1a13d7794342944bd
AyushDubey-BellBlaze <ayush.dubey@bellblazetech.com>
2026-01-30 10:21:02 +0530

    Add files via upload

---
+++ Assignment1.py
@@ -0,0 +1,22 @@
+list = []
+list.append(3)
+list.append(7)
+list.append(9)
+list.append(-5)
+list.insert(2, 11)
+print(list)
+
+list.remove(7)
+print(list)
+
+list.append(8)
+print(list)
+
+list.sort()
+print(list)
+
+list.pop()
+print(list)
+
+list.reverse()
+print(list)
